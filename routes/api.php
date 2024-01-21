214c39611054ca89f40db14454c394b8728166e1
Fabio Sozzi <fabio.sozzi@gmail.com>
2024-01-21 21:18:40 +0000

    First test written

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name('api.')->group(function() {
+    Route::prefix('user')->name('user.')->group(function() {
+        Route::middleware('auth:sanctum')->get('/', function (Request $request) {
+            return $request->user();
+        })->name('get');
+    });
 });
