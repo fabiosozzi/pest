ff93d5dd64c6bdb960500844620d81d41696591b
Fabio Sozzi <fabio.sozzi@gmail.com>
2024-02-01 21:36:13 +0100

    Product test rewritten

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -8,11 +8,8 @@ use App\Models\Product;
 
 describe('API', function () {
     it('can fetch info of a product with the API "Get Product" route', function () {
-        $category = Category::factory()
-            ->has(Product::factory()->count(1))
-            ->create();
-
-        $product = new ProductResource($category->products->first());
+        $category = Category::factory()->create();
+        $product = new ProductResource(Product::factory()->for($category)->create());
         $response = $this->get(route('api.product.get', ['product' => $product->id]));
         $response->assertStatus(200)->assertJson(['data' => $product->resolve()]);
     });
